ab8f0c5176b2f1aae4b4d50fb21b7f4e22a8c484
ouedraogoosias4@gmail.com <ouedraogoosias4@gmail.com>
2026-02-17 14:46:41 +0100

    second commit integration express

--- servers.js
+++ servers.js
@@ -1,28 +1,39 @@
-import http from "node:http";
-import { Json } from "./src/utils/responses.js";
-import { listUsers } from "./src/users/users.controlers.js";
+// import http from "node:http";
+// import { Json } from "./src/utils/responses.js";
+// import { listUsers } from "./src/users/users.controlers.js";
 
-const listener = (request, response) => {
-
-
-    if (request.url === "/users") {
-    return listUsers(request, response);
-  }
-
-  return Json(response, 404, {
-    message: "Not found",
-  });
-  // json(response, 200, {
-  //   message: "API works!!"
-  // }) ; 
-
-  // response.writeHead(200, { "content-type": "text/plain" });
-  // response.end("hello word\n");
-};
-
-const server = http.createServer(listener);
-server.listen(3000);
-
-console.log("server running at http://127.0.0.1:3000/");
+// const listener = (request, response) => {
 
 
+//     if (request.url === "/users") {
+//     return listUsers(request, response);
+//   }
+
+//   return Json(response, 404, {
+//     message: "Not found",
+//   });
+//   // json(response, 200, {
+//   //   message: "API works!!"
+//   // }) ; 
+
+//   // response.writeHead(200, { "content-type": "text/plain" });
+//   // response.end("hello word\n");
+// };
+
+// const server = http.createServer(listener);
+// server.listen(3000);
+
+// console.log("server running at http://127.0.0.1:3000/");
+
+
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Users Management API')
+})
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000')
+})
--- src/utils/responses.js
+++
@@ -1,6 +0,0 @@
-
-export function Json(response, status, data){
-  response.writeHead(status, { "content-type": "text/plain" });
-  response.end(JSON.stringify(data));
-};
-
